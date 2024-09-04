788c49e5b640c0e87d70a3ad5bfe7972e566047e
chonkov <georgi.chonkov27@gmail.com>
2024-09-04 19:06:05 +0300

    Init 'Vault' setup

---
+++ src/Vault.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import {IERC4626} from "lib/openzeppelin-contracts/contracts/interfaces/IERC4626.sol";
+
+abstract contract Vault is IERC4626 {}
