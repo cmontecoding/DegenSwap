f1d2d1647e57339e123bf18b87d71a97e4523ae1
chonkov <georgi.chonkov27@gmail.com>
2024-09-06 00:34:00 +0300

    Refactored 'Vault' contract

--- src/Vault.sol
+++ src/Vault.sol
@@ -2,29 +2,35 @@
 pragma solidity 0.8.26;
 
 import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Ownable, Ownable2Step} from "lib/openzeppelin-contracts/contracts/access/Ownable2Step.sol";
 
 import {IVault} from "../src/interfaces/IVault.sol";
 
 abstract contract Vault is IVault, Ownable2Step {
+    using SafeERC20 for IERC20;
+
     uint256 public constant BPS = 10_000;
 
-    // (pool => (user => (token => amount))) balances;
+    // (user => (pool => (token => amount))) balances;
     mapping(address user => mapping(address token => uint256 amount)) public balances;
+
+    // (pool => (token => amount)) balances;
     mapping(address token => uint256 amount) public totalSupply;
 
     uint256 public fee; // bps
 
     constructor(address _owner, uint256 _fee) Ownable(_owner) {
-        require(_fee <= BPS);
+        require(_fee <= BPS, InvalidFee(_fee));
         fee = _fee;
     }
 
     function setFee(uint256 newFee) external onlyOwner {
-        require(newFee <= BPS);
+        require(newFee <= BPS, InvalidFee(newFee));
+        uint256 oldFee = fee;
         fee = newFee;
 
-        // emit EVENT(...);
+        emit ChangedFee(oldFee, newFee);
     }
 
     function addLiquidity(address token, uint256 amount) external {
@@ -36,13 +42,12 @@ abstract contract Vault is IVault, Ownable2Step {
     }
 
     function _addLiquidity(address token, uint256 amount) internal {
-        // note: use `safeTransferFrom`
-        IERC20(token).transferFrom(msg.sender, address(this), amount);
+        IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
 
         balances[msg.sender][token] += amount;
         totalSupply[token] += amount;
 
-        //   emit EVENT(...);
+        emit AddedLiquidity(msg.sender, token, amount);
     }
 
     function _removeLiquidity(address token, uint256 amount) internal {
@@ -53,10 +58,9 @@ abstract contract Vault is IVault, Ownable2Step {
             totalSupply[token] -= amount;
         }
 
-        // note: use `safeTransfer`
-        IERC20(token).transfer(msg.sender, amount);
+        IERC20(token).safeTransfer(msg.sender, amount);
 
-        //   emit EVENT(...);
+        emit RemovedLiquidity(msg.sender, token, amount);
     }
 
     function getFee(uint256 amount) public view returns (uint256) {
--- src/interfaces/IVault.sol
+++ src/interfaces/IVault.sol
@@ -2,6 +2,12 @@
 pragma solidity 0.8.26;
 
 interface IVault {
+    error InvalidFee(uint256 proposedFee);
+
+    event ChangedFee(uint256 oldFee, uint256 newFee);
+    event AddedLiquidity(address indexed user, address indexed token, uint256 amount);
+    event RemovedLiquidity(address indexed user, address indexed token, uint256 amount);
+
     function addLiquidity(address token, uint256 amount) external;
     function removeLiquidity(address token, uint256 amount) external;
 }
