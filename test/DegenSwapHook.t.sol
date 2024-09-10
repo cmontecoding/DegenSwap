f94c1a3dc3f1fd6acbbc2d052656533da4f788cb
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2024-09-09 23:00:43 -0400

    test setup

---
+++ test/DegenSwapHook.t.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Test} from "forge-std/Test.sol";
+
+import {Deployers} from "@uniswap/v4-core/test/utils/Deployers.sol";
+import {PoolSwapTest} from "v4-core/test/PoolSwapTest.sol";
+import {MockERC20} from "solmate/src/test/utils/mocks/MockERC20.sol";
+
+import {PoolManager} from "v4-core/PoolManager.sol";
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+
+import {Currency, CurrencyLibrary} from "v4-core/types/Currency.sol";
+
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+import {TickMath} from "v4-core/libraries/TickMath.sol";
+import {SqrtPriceMath} from "v4-core/libraries/SqrtPriceMath.sol";
+import {LiquidityAmounts} from "@uniswap/v4-core/test/utils/LiquidityAmounts.sol";
+
+import "forge-std/console.sol";
+import {DegenSwapHook} from "../src/DegenSwapHook.sol";
+
+import {VRFCoordinatorV2_5Mock} from "chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
+
+contract TestPointsHook is Test, Deployers {
+    using CurrencyLibrary for Currency;
+
+    MockERC20 token; // our token to use in the ETH-TOKEN pool
+
+    // Native tokens are represented by address(0)
+    Currency ethCurrency = Currency.wrap(address(0));
+    Currency tokenCurrency;
+
+    DegenSwapHook hook;
+    VRFCoordinatorV2_5Mock vrfCoordinator;
+
+    function setUp() public {
+        vrfCoordinator = new VRFCoordinatorV2_5Mock(100, 100, 100);
+
+        // Step 1 + 2
+        // Deploy PoolManager and Router contracts
+        deployFreshManagerAndRouters();
+
+        // Deploy our TOKEN contract
+        token = new MockERC20("Test Token", "TEST", 18);
+        tokenCurrency = Currency.wrap(address(token));
+
+        // Mint a bunch of TOKEN to ourselves and to address(1)
+        token.mint(address(this), 1000 ether);
+        token.mint(address(1), 1000 ether);
+
+        // Deploy hook to an address that has the proper flags set
+        uint160 flags = uint160(Hooks.BEFORE_SWAP_FLAG);
+        deployCodeTo(
+            "DegenSwapHook.sol",
+            abi.encode(manager, "Degen Swap Token", "DST", address(0)),
+            address(flags)
+        );
+
+        // Deploy our hook
+        hook = DegenSwapHook(address(flags));
+
+        // Approve our TOKEN for spending on the swap router and modify liquidity router
+        // These variables are coming from the `Deployers` contract
+        token.approve(address(swapRouter), type(uint256).max);
+        token.approve(address(modifyLiquidityRouter), type(uint256).max);
+
+        // Initialize a pool
+        (key, ) = initPool(
+            ethCurrency, // Currency 0 = ETH
+            tokenCurrency, // Currency 1 = TOKEN
+            hook, // Hook Contract
+            3000, // Swap Fees
+            SQRT_PRICE_1_1, // Initial Sqrt(P) value = 1
+            ZERO_BYTES // No additional `initData`
+        );
+    }
+
+    function testRequestRandomness() public {
+        // hook.getRandomness(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+    }
+}
